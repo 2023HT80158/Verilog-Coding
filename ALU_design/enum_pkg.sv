f9ed4bcd2bac2e33c8499a83d254c21f2624874b
2023HT80158 <2023ht80158@wilp.bits-pilani.ac.in>
2025-09-26 22:45:33 +0530

    Create enum_pkg.sv

---
+++ ALU_design/enum_pkg.sv
@@ -0,0 +1,13 @@
+package enum_pkg;
+  typedef enum logic [3:0] {
+    Add,
+    Sub,
+    Leftshift,
+    RightshiftArith,
+    RightshiftLogic,
+    And,
+    Or,
+    Xor,
+    Equal
+  } Opcode;
+endpackage
